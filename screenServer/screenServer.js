e58af5f7eedecfd995bb9928d476860206fd7ff1
georgecode <georgeczurlanis@gmail.com>
2020-05-02 07:21:47 -0700

    Add screenServer

--- screenServer/screenServer.js
+++ screenServer/screenServer.js
@@ -1,8 +1,18 @@
-
-
 const http =require('http')
 const port = 3000
 
-const server = http.
+const server = http.createServer(function(req,res){
+	res.writeHead(200,{'Content-Type':'text/html'})
+	res.write('screenServer.js says yo')
+	res.end()
+})
+
+server.listen(port,function(error){
+	if(error){
+		console.log('shits broke',error)
+	}else{
+		console.log('Server is listing on port'+ port)
+	}
+})
 
 console.log("hello testtttt")
