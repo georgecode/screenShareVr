f3d96f503771f1f924664ba957f3161612963c59
georgecode <georgeczurlanis@gmail.com>
2020-05-02 05:48:22 -0700

    first commit

---
+++ screenServer/screenServer.js
@@ -0,0 +1,8 @@
+
+
+const http =require('http')
+const port = 3000
+
+const server = http.
+
+console.log("hello testtttt")
---
+++ screenServer/ui_script.js
@@ -0,0 +1,4 @@
+  if (!navigator.mediaDevices.getUserMedia) {
+  	alert("something is fucked up prob need to enable in your browser, something to do with getUserMedia or webRTC ")
+    //document.getElementById("gum_warn").style.display = "none";
+  }
